ebee8d0c17f910efb6e74cb2c5a0752eb72f1c74
Talles Souza <tallessouza117@gmail.com>
2025-06-09 22:02:26 -0300

    2ª aula de ALG

---
+++ Grupo_A/A1_6Teste.java
@@ -0,0 +1,12 @@
+package Grupo_A;
+
+public class A1_6Teste {
+   public static void main(String[] args) {
+   
+       // String (não é um tipo primitivo)
+       String texto = "Exemplo de texto";
+
+       // Exibindo os valores
+       System.out.println("String: " + texto);
+   }
+}
