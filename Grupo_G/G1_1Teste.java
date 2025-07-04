da32d759ec9eaadf7c9f4dfabea10dcea8eaa96c
Talles Souza <tallessouza117@gmail.com>
2025-07-03 22:00:21 -0300

    última aula de java

---
+++ Grupo_G/G1_1Teste.java
@@ -0,0 +1,8 @@
+package Grupo_G;
+
+public class G1_1Teste {
+    public static void main(String[] args) {
+        String nome = "teste";
+        System.out.println("String literal = " + nome);
+    }
+}
---
+++ Grupo_G/G1_5Teste.java
@@ -0,0 +1,9 @@
+package Grupo_G;
+
+public class G1_5Teste {
+    public static void main(String[] args) {
+        String str = "abcdefghi";
+        int tamanho = str.length();
+        System.out.println("O tamanho da string " + str + " vale " + tamanho);
+    }
+}
